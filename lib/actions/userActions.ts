d861daca2fdbb9ab4295edf77c55c0dc1cf3aae6
nonsodaniel <nonsodaniel07@gmail.com>
2023-10-23 00:21:58 +0100

    Implemented activity page

--- lib/actions/userActions.ts
+++ lib/actions/userActions.ts
@@ -147,3 +147,32 @@ export async function fetchUserPosts(userId: string) {
     throw error;
   }
 }
+
+export async function getActivity(userId: string) {
+  try {
+    DBConnection();
+
+    // Find all posts created by the user
+    const userPosts = await Post.find({ author: userId });
+
+    // Collect all the child Post ids (replies) from the 'children' field of each user Post
+    const childPostIds = userPosts.reduce((acc, userPost) => {
+      return acc.concat(userPost.children);
+    }, []);
+
+    // Find and return the child Posts (replies) excluding the ones created by the same user
+    const replies = await Post.find({
+      _id: { $in: childPostIds },
+      author: { $ne: userId }, // Exclude posts authored by the same user
+    }).populate({
+      path: "author",
+      model: User,
+      select: "name image _id",
+    });
+
+    return replies;
+  } catch (error) {
+    console.error("Error fetching replies: ", error);
+    throw error;
+  }
+}
