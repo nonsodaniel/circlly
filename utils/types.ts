6edc7da66489b72997412b25686340e4d85bc086
nonsodaniel <nonsodaniel07@gmail.com>
2023-10-22 01:19:18 +0100

    implemented Posts fetching and display in the view

--- utils/types.ts
+++ utils/types.ts
@@ -6,3 +6,27 @@ export interface IUserProps {
   bio: string;
   image: string;
 }
+
+export interface IPostCardProps {
+  id: string;
+  currentUserId: string;
+  parentId: string | null;
+  content: string;
+  author: {
+    name: string;
+    image: string;
+    id: string;
+  };
+  community: {
+    id: string;
+    name: string;
+    image: string;
+  } | null;
+  createdAt: string;
+  comments: {
+    author: {
+      image: string;
+    };
+  }[];
+  isComment?: boolean;
+}
