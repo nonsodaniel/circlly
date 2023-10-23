879c2b1fae5e58f0e1685e3db252189d2a2f1274
nonsodaniel <nonsodaniel07@gmail.com>
2023-10-23 11:48:18 +0100

    Implemented community list page

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,7 +1,63 @@
-import React from "react";
+import CommunityCard from "@/app/components/cards/CommunityCard";
+import Pagination from "@/app/components/shared/Pagination";
+import Searchbar from "@/app/components/shared/Searchbar";
+import { fetchCommunities } from "@/lib/actions/communityActions";
+import { fetchUser } from "@/lib/actions/userActions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const page = () => {
-  return <div>Community page</div>;
-};
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const user = await currentUser();
+  if (!user) return null;
 
-export default page;
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const result = await fetchCommunities({
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
+    pageSize: 25,
+  });
+
+  return (
+    <>
+      <h1 className="head-text">Communities</h1>
+
+      <div className="mt-5">
+        <Searchbar routeType="communities" />
+      </div>
+
+      <section className="mt-9 flex flex-wrap gap-4">
+        {result.communities.length === 0 ? (
+          <p className="no-result">No Result</p>
+        ) : (
+          <>
+            {result.communities.map((community) => (
+              <CommunityCard
+                key={community.id}
+                id={community.id}
+                name={community.name}
+                username={community.username}
+                imgUrl={community.image}
+                bio={community.bio}
+                members={community.members}
+              />
+            ))}
+          </>
+        )}
+      </section>
+
+      <Pagination
+        path="communities"
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
+    </>
+  );
+}
+
+export default Page;
